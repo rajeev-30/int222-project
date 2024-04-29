2806b9c094948b4f28d2784a9bcc26e7084794ec
Rajeev Kushwaha <rajeevkushwaha1812@gmail.com>
2024-04-29 17:18:32 +0530

    made some changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://int222-project.vercel.app";
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-//   BASE_URL = "http://localhost:4000/";
-// }
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  BASE_URL = "http://localhost:4000/";
+}
 
 export { BASE_URL };
